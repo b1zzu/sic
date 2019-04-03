edae22feff89712f49bd831e74d73f0eca548362
Davide Bizzarri <david3bizzarri@gmail.com>
2019-04-03 17:41:41 +0200

    Add clap to parse command args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
+use clap::{App, SubCommand};
+
+use crate::commands::cards::cards;
+
 mod database;
 mod utils;
 mod commands;
 
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("sic")
+        .version("1.0")
+        .about("Command Line Tool for SafeInCloud")
+        .subcommand(SubCommand::with_name("cards")
+            .about("print all cards"))
+        .get_matches();
+
+    if let Some(_) = matches.subcommand_matches("cards") {
+        cards();
+    }
 }
