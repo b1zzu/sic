7925f08463d9b0e5551c10f0b34fc0c1548fba2f
Davide Bizzarri <david3bizzarri@gmail.com>
2019-04-04 02:04:32 +0200

    Add database args to cli

--- src/commands/cards.rs
+++ src/commands/cards.rs
@@ -1,13 +1,18 @@
-pub fn cards() {
+use std::path::PathBuf;
+
+pub fn cards(database: PathBuf) {
     // TODO:
 }
 
 #[cfg(test)]
 mod tests {
+    use std::path::Path;
+
     use super::*;
 
     #[test]
     fn test_cards() {
-        cards()
+        let database = Path::new("./samples/SafeInCloud.dh");
+        cards(database.to_path_buf());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use clap::{App, SubCommand};
+use std::env;
+use std::path::{Path, PathBuf};
+
+use clap::{App, Arg, SubCommand};
 
 use crate::commands::cards::cards;
 
@@ -10,11 +13,21 @@ fn main() {
     let matches = App::new("sic")
         .version("1.0")
         .about("Command Line Tool for SafeInCloud")
+        .arg(Arg::with_name("db")
+            .long("db")
+            .value_name("FILE")
+            .help("Set a custom path to the SafeInCloud db")
+            .takes_value(true))
         .subcommand(SubCommand::with_name("cards")
             .about("print all cards"))
         .get_matches();
 
+    let database = match matches.value_of("db") {
+        Some(database) => Path::new(database).to_path_buf(),
+        None => dirs::home_dir().unwrap_or(PathBuf::new()).join("./.SafeInCloud.db"),
+    };
+
     if let Some(_) = matches.subcommand_matches("cards") {
-        cards();
+        cards(database);
     }
 }
