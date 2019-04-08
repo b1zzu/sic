df69596bb3b825d2106bb0555f9ee29b7399a0f1
Davide Bizzarri <david3bizzarri@gmail.com>
2019-04-08 17:58:17 +0200

    Implement format table

--- src/utils/format.rs
+++ src/utils/format.rs
@@ -1,7 +1,44 @@
 type Table = Vec<Vec<(Option<String>, String)>>;
 
 fn table(table: Table) -> String {
-    "".to_string()
+    let mut measurements: Vec<(usize, usize)> = Vec::new();
+    for row in &table {
+        for (i, (left, right)) in row.iter().enumerate() {
+            let left = match left {
+                Some(left) => left.len() + 2,
+                None => 0
+            };
+            let right = right.len();
+            let measurement = measurements.get(i);
+            match measurement {
+                Some((left_m, right_m)) => {
+                    let left = if left > *left_m { left } else { *left_m };
+                    let right = if right > *right_m { right } else { *right_m };
+                    measurements[i] = (left, right);
+                }
+                None => {
+                    measurements.push((left, right));
+                }
+            }
+        }
+    }
+    let measurements = measurements;
+
+    let mut rows = Vec::new();
+    for row in table {
+        let mut columns = Vec::new();
+        for (i, (left, right)) in row.iter().enumerate() {
+            let (left_m, right_m) = measurements[i];
+            let left = match left {
+                Some(left) => format!("{}: ", left),
+                None => String::new(),
+            };
+            columns.push(format!("{0:1$}{2:3$}", left, left_m, right, right_m));
+        }
+        rows.push(columns.join(" | "))
+    }
+
+    rows.join("\n")
 }
 
 #[cfg(test)]
@@ -18,7 +55,7 @@ mod test {
         ];
 
         let expected = "\
- 1 | Facebook | Login:    fantastic@email.com
+1  | Facebook | Login:    fantastic@email.com
 27 | GMail    | Password: *******            ";
 
         assert_eq!(table(t), expected);
