225752dca5f8cd87f7f8e65e4887bcb3bc6701e2
Davide Bizzarri <david3bizzarri@gmail.com>
2019-04-08 16:31:29 +0200

    Add test for the open function

--- src/database/helper.rs
+++ src/database/helper.rs
@@ -16,3 +16,20 @@ pub fn open(database: PathBuf, password: Option<String>) -> Vec<Card> {
 
     parse(database.as_slice())
 }
+
+#[cfg(test)]
+mod tests {
+    use std::fs::File;
+    use std::path::Path;
+
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let database = Path::new("./samples/SafeInCloud.db").to_path_buf();
+        let password = "TheHarde5tPassw@ord!nT#3World".to_string();
+        let database = open(database, Some(password));
+
+        assert_eq!(database.len(), 5);
+    }
+}
