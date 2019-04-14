e195642bcae923b97fb732b3a7f39fa4f84688e1
Davide Bizzarri <david3bizzarri@gmail.com>
2019-04-14 08:25:34 +0200

    Add attributes to fuzzy

--- src/utils/fuzzy.rs
+++ src/utils/fuzzy.rs
@@ -1,11 +1,23 @@
-pub fn fuzzy() {}
+pub trait Searchable {
+    fn value(&self) -> &str;
+}
+
+pub fn fuzzy<T: Searchable>(stack: &[T], needle: &str) -> Vec<T> {
+    vec![]
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
 
+    impl Searchable for &str {
+        fn value(&self) -> &str {
+            &self
+        }
+    }
+
     #[test]
     fn test_fuzzy() {
-        fuzzy();
+        fuzzy(&["one", "two"], "wo");
     }
 }
