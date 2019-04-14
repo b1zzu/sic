1bc866cff02627c0037ff9a3d94e36fc8d32851f
Davide Bizzarri <david3bizzarri@gmail.com>
2019-04-14 08:16:27 +0200

    Create emtpy fuzzy search function

---
+++ src/utils/fuzzy.rs
@@ -0,0 +1,11 @@
+pub fn fuzzy() {}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_fuzzy() {
+        fuzzy();
+    }
+}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,3 +2,4 @@ pub mod byte_read;
 pub mod format;
 pub mod option;
 pub mod password;
+pub mod fuzzy;
