917f0937d4adf4318ee89e4a32121ec723ba0b0c
Davide Bizzarri <david3bizzarri@gmail.com>
2019-04-14 09:15:48 +0200

    Impl fuzzy search algorithm
    missing key insensitive

--- src/utils/fuzzy.rs
+++ src/utils/fuzzy.rs
@@ -2,8 +2,47 @@ pub trait Searchable {
     fn value(&self) -> &str;
 }
 
-pub fn fuzzy<T: Searchable>(stack: &[T], needle: &str) -> Vec<T> {
-    vec![]
+struct S<T> {
+    last: usize,
+    points: usize,
+    object: T,
+}
+
+impl<T> S<T> {
+    fn new(object: T) -> Self {
+        S { last: 0, points: 0, object }
+    }
+}
+
+pub fn fuzzy<'a, T: Searchable>(stack: &'a [T], needle: &str) -> Vec<&'a T> {
+    let mut r = vec![];
+    for o in stack {
+        let mut s = S::new(o);
+        let mut v = o.value();
+        for n in needle.chars() {
+            match v.find(n) {
+                Some(p) => {
+                    s.points += if s.last + 1 == p {
+                        p - s.last
+                    } else {
+                        p
+                    };
+                    s.last = p;
+                }
+                None => {
+                    s.points = 0;
+                    break;
+                }
+            }
+        }
+        if s.points != 0 {
+            r.push(s);
+        }
+    }
+
+    r.sort_unstable_by_key(|s| s.points);
+
+    r.iter().map(|s| s.object).collect()
 }
 
 #[cfg(test)]
@@ -19,6 +58,12 @@ mod test {
     #[test]
     fn test_fuzzy() {
         let result = fuzzy(&["Facebook", "Twitter", "Baked"], "ak");
-        assert_eq!(result, vec!["Baked", "Facebook"]);
+        assert_eq!(result, vec![&"Baked", &"Facebook"]);
+    }
+
+    #[test]
+    fn test_fuzzy_same_match_different_position() {
+        let result = fuzzy(&["Facebook", "Book", "other"], "book");
+        assert_eq!(result, vec![&"book", &"Facebook"])
     }
 }
